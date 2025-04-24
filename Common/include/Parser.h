ee99ee459756982ed429993958aa34c466aa175c
Coch <constantin.verine@gmail.com>
2025-04-24 19:35:16 +0200

    new struct item

--- Common/include/Parser.h
+++ Common/include/Parser.h
@@ -3,6 +3,17 @@
 #include "FileSytem.h"
 #include "Metrics.h"
 
+struct Item
+{
+	std::string Id;
+	std::string ImagePath;
+	std::string Name;
+	int Quantity = 0;
+	int Price = 0;
+	bool isStackable = true;
+
+};
+
 struct Parser
 {
 	std::vector<std::vector<std::string>> Data;
